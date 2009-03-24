e5ae6b614a67febabea8f2b4dccf6f84a6a64ebe
Eli Van Zoeren <design@elivz.com>
2009-03-24 15:01:53 -0400

    Move javascript into the display_field() function using insert_js() rather than custom hook

--- extensions/fieldtypes/ff_vz_url/ft.ff_vz_url.php
+++ extensions/fieldtypes/ff_vz_url/ft.ff_vz_url.php
@@ -19,19 +19,17 @@ class Ff_vz_url extends Fieldframe_Fieldtype {
 	 */
 	var $info = array(
 		'name'             => 'VZ URL',
-		'version'          => '0.9.7',
+		'version'          => '0.9.8',
 		'desc'             => 'Textbox with ajax url validation',
 		'docs_url'         => 'http://elivz.com/blog/single/vz_url_extension/',
 		'versions_xml_url' => 'http://elivz.com/files/version.xml'
 	);
 	
 	var $requires = array(
-		'ff'        => '0.9.5',
+		'ff'        => '0.9.6',
 		'cp_jquery' => '1.1',
 	);
     
-	var $hooks = array('publish_form_headers');
-
 	var $default_site_settings = array('vz_url_error_text' => "Please check your url, as it seems to be invalid.");
 
 
@@ -51,24 +49,7 @@ class Ff_vz_url extends Fieldframe_Fieldtype {
 		$r .= $SD->block_c();
 		return $r;
 	}
-
-
-	/**
-	* Add script to validate the urls to the header
-	*/
-	function publish_form_headers()
-	{
-		$r = $this->get_last_call('').NL.NL;
-
-		$r .= "<script type='text/javascript' charset='utf-8'>$(document).ready(function() {
-	$('.vz_url_field').blur(function() {
-		field = $(this);
-		$.get( '".FT_URL."ff_vz_url/proxy.php', {path: field.val()}, function(response) { if (response) { field.next().slideUp(); } else { field.next().slideDown(); } });
-	});
-});</script>".NL.NL;
-
-		return $r;
-	}
+	
     
 	/**
 	 * Display Field
@@ -80,6 +61,13 @@ class Ff_vz_url extends Fieldframe_Fieldtype {
 	 */
 	function display_field($field_name, $field_data, $field_settings)
 	{
+		$this->insert_js("$(document).ready(function() {
+	$('.vz_url_field').blur(function() {
+		field = $(this);
+		$.get( FT_URL+'ff_vz_url/proxy.php', {path: field.val()}, function(response) { if (response) { field.next().slideUp(); } else { field.next().slideDown(); } });
+	});
+});");
+
 		$SD = new Fieldframe_SettingsDisplay();
 		
 		// Fill in http:// if the field is empty
@@ -94,7 +82,7 @@ class Ff_vz_url extends Fieldframe_Fieldtype {
 	/**
 	 * Save Field
 	 * 
-	 * @param  string  $field_name		The field's name
+	 * @param  string  $field_data		The field's post data
 	 * @param  array  $field_settings	The field settings
 	 */
 	function save_field($field_data, $field_settings)
